6c252c9dfe984ece6991cba43dea058380b1a137
curgery <androidgeek54@gmail.com>
2020-09-16 16:25:17 -0400

    removed unused dependents

--- App.js
+++ App.js
@@ -1,16 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
 import Greeting from './components/greeting';
+
 export default function App() {
   return <Greeting />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
